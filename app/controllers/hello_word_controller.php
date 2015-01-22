307a4f64ad4859791fd92840f98198dd58acecd0
Kaltsoon <Kalle.ilves@helsinki.fi>
2015-01-22 08:51:15 +0200

    kontrollereiden siistimistä ja aloitussivun päivittäminen

--- app/controllers/hello_word_controller.php
+++ app/controllers/hello_word_controller.php
@@ -3,6 +3,7 @@
   class HelloWorldController extends BaseController{
 
     public static function index(){
+      // make-metodi renderöi app/views-kansiossa sijaitsevia tiedostoja
    	  View::make('home.html');
     }
 
