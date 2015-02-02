f290d2528f9bb477ec74ba8b953ad2ef32e40eef
Kaltsoon <Kalle.ilves@helsinki.fi>
2015-02-02 11:29:58 +0200

    mm. environment konffi muutettu

--- config/environment.sh
+++ config/environment.sh
@@ -2,5 +2,5 @@
 # korvaa tämä omalla käyttäjätunnuksellasi
 USERNAME="kalleilv"
 # korvaa tämä haluamallasi kansion nimellä
-PROJECT_FOLDER="tsoha-testi"
+PROJECT_FOLDER="tsoha"
 # sovelluksesi tulee sijaitsemaan osoitteessa USERNAME.users.cs.helsinki.fi/PROJECT_FOLDER
