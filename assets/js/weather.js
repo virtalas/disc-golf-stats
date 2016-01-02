636b547a44dbcb6fcddb167b28f0740b771a84c5
virtalas <virtalas@users.noreply.github.com>
2016-01-02 19:23:59 +0200

    temperature rounding

--- assets/js/weather.js
+++ assets/js/weather.js
@@ -9,6 +9,8 @@ $(document).ready(function () {
   $.ajax({
     url: "http://api.openweathermap.org/data/2.5/weather?q=" + city + "," + country + "&units=metric&appid=076ac060620f271da6d88921f859c5dd"
   }).done(function(data) {
-    $("#temperature").text(data.main.temp + " °C");
+    console.log("temperature: " + data.main.temp);
+    var temperature = data.main.temp.toFixed(1); // round to 1 decimal place
+    $("#temperature").text(temperature + " °C");
   });
 });
