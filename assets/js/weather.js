50fa9a1200dcee625ed5c1e842d273727b3a5a5d
virtalas <virtalas@users.noreply.github.com>
2016-01-02 19:35:29 +0200

    added wind speed

--- assets/js/weather.js
+++ assets/js/weather.js
@@ -10,7 +10,12 @@ $(document).ready(function () {
     url: "http://api.openweathermap.org/data/2.5/weather?q=" + city + "," + country + "&units=metric&appid=076ac060620f271da6d88921f859c5dd"
   }).done(function(data) {
     console.log("temperature: " + data.main.temp);
+    console.log("wind: " + data.wind.speed)
+
     var temperature = data.main.temp.toFixed(1); // round to 1 decimal place
+    var wind = Math.round(data.wind.speed);
+
     $("#temperature").text(temperature + " °C");
+    $("#wind").text(wind + " m/s");
   });
 });
