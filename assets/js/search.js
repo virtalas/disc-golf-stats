7fb8d0dd5c3e70e64c858f62b8b7d2cfc6e2f8c3
virtalas <virtalas@users.noreply.github.com>
2017-06-02 18:19:24 +0300

    select list for conditions

--- assets/js/search.js
+++ assets/js/search.js
@@ -6,12 +6,30 @@ $("#search-toggle").click(function() {
     $("#result").html("Haetaan pelejä...");
     $("#title").html("Hae pelejä");
     $("#search-toggle").html("Lopeta haku");
+    $("#search-toggle").attr("href", "/game");
     $("#search-attributes").css("display", "inline");
+
+    $('#rain').on('click', function() {
+      console.log("clsl");
+    });
 
     $.get('game/search', function(data) {
       $('#result').html(data);
     });
+
+    var refreash = function() {
+      $.post( 'game/search', $('form#conditions').serialize(), function(data) {
+        $('#result').html(data);
+      });
+      console.log("change");
+    }
+
+    $('#rain').on('click', function() {
+      console.log("clsl");
+    });
+
   } else {
     // redirect to /game
+    location.reload();
   }
 });
