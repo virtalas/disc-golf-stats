5a94edf539b5bd2aa053da590284e55b05b73c17
virtalas <virtalas@users.noreply.github.com>
2017-12-23 18:44:45 +0200

    basic course tests, bash file for generating code coverage

--- tests/unit/CourseCest.php
+++ tests/unit/CourseCest.php
@@ -1,19 +1,44 @@
 <?php
 
 
-class CourseCest{
+class CourseCest {
 
     private $course;
 
-    public function _before(UnitTester $I){
+    public function _before(UnitTester $I) {
         $this->course = new Course(array(
-            'name' => $params['name'],
-            'city' => $params['city'],
-            'map' => $params['map']));
+            'name' => "Hirvikuja",
+            'city' => "Helsinki",
+            'map' => "https://kartta.com/123"));
+
+        $this->course->save();
     }
 
-    public function _after(UnitTester $I){
+    public function _after(UnitTester $I) {
+        $this->course->destroy();
     }
 
-    
+    public function courseCanBeSaved(UnitTester $I) {
+        $I->seeInDatabase('course', ['name' => "Hirvikuja", 'city' => "Helsinki", 'map' => "https://kartta.com/123", "courseid" => $this->course->courseid]);
+    }
+
+    public function courseCanBeUpdated(UnitTester $I) {
+        $this->course->name = "Hirvimetsä";
+        $this->course->city = "Espoo";
+        $this->course->map = "";
+        $this->course->update();
+        $I->seeInDatabase('course', ['name' => "Hirvimetsä", 'city' => "Espoo", 'map' => "", "courseid" => $this->course->courseid]);
+    }
+
+    public function courseCanBeDestroyed(UnitTester $I) {
+        $this->course->destroy();
+        $I->dontSeeInDatabase('course', ['name' => "Hirvimetsä", 'city' => "Espoo", 'map' => "", "courseid" => $this->course->courseid]);
+    }
+
+    public function courseCanBeFoundById(UnitTester $I) {
+        $found_course = Course::find($this->course->courseid);
+        $I->assertEquals($this->course->courseid, $found_course->courseid);
+        $I->assertEquals($this->course->name, $found_course->name);
+        $I->assertEquals($this->course->map, $found_course->map);
+    }
 }
